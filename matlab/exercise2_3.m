04694a8b61b0b7d22a83df26be8204ab47aeb2ee
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-05-13 22:02:06 +0200

    Ex2.3: Fix u for each realization. Added more visually appealing simulations.

--- matlab/exercise2_3.m
+++ matlab/exercise2_3.m
@@ -22,13 +22,29 @@ disp(" ");
 disp("a) Plot three realizations with t=[1..150]. Is it consistent with the theoretical result?");
 
 t = 1:150;
+u = unifrnd(-pi, pi, 3, 1);
+[t, u] = meshgrid(t,u);
 
-Y = 1*sin(pi/5*t + unifrnd(-pi, pi, 3, 150)) + normrnd(0, 1, 3, 150);
+Y = 1*sin(pi/5*t + u) + normrnd(0, 1, 3, 150);
 
-plot(t, Y(1,:), t, Y(2,:), t, Y(3,:));
+plot(t(1,:), Y(1,:), t(2,:), Y(2,:), t(3,:), Y(3,:));
+title("Realizations of Y(t)");
+xlabel("t");
+ylabel("Y(t)");
 
 disp("Mean of each simulation: " + mean(Y(1,:)) +", "+ mean(Y(2,:)) +", "+  mean(Y(3,:)));
 
 % The plot shows that the values do indeed oscillate around 0.
 
+figure(2);
 
+t = 1:150;
+u = unifrnd(-pi, pi, 3, 1);
+[t, u] = meshgrid(t,u);
+
+Y = 1*sin(pi/50*t + u) + normrnd(0, 0.2, 3, 150);
+
+plot(t(1,:), Y(1,:), t(2,:), Y(2,:), t(3,:), Y(3,:));
+title("Realizations of Y(t) with tweaked parameters");
+xlabel("t");
+ylabel("Y(t)");
